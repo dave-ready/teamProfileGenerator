26f445b95ba2f53f3dc6ec0ae5856143787951e9
dave ready <davis_ready@hotmail.com>
2021-01-26 18:43:52 -0500

    more requires + js files

--- index.js
+++ index.js
@@ -4,5 +4,9 @@ const inquirer = require('inquirer');
 const jest = require('jest');
 const fs = require('fs');
 const util = require('util');
+const manager = require('./lib/manager');
+const engineer = require('./lib/engineer');
+const intern = require('./lib/intern');
+const employees = [];
 
 const writeFileAsync = util.promisify(fs.writeFile);
