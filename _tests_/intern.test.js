26b31d6bad6e09de3847fda54acda9d960e6e8e3
dave ready <davis_ready@hotmail.com>
2021-02-01 23:01:36 -0500

    intern.test.js file

---
+++ _tests_/intern.test.js
@@ -0,0 +1,41 @@
+const Intern = require("../lib/Intern");
+
+describe("Intern", () => {
+    it("Will create a new Intern", () => {
+        const int = new Intern();
+        expect(typeOf(int)).toBe("object");
+    });
+
+
+    it("Will get Intern's school using constructor function", () => {
+        const userSchool = "Columbia";
+        const int = new Intern("Dylan", 25, "test_email@testmail.com", userSchool);
+        expect(int.school).toBe(userSchool);
+      });
+
+describe("getRole", () => {
+    it("Will return 'Intern' using getRole function", () => {
+        const intRole = "Intern";
+        const int = new Intern("Dylan", 25, "test_email@testmail.com", "Columbia");
+        expect(int.getRole()).toBe(intRole);
+    });
+    
+});
+    
+describe("getSchool", () => {
+    it("Will get Intern's school using getSchool function", () => {
+        const userSchool = "Columbia";
+        const int = new Intern("Dylan", 25, "test_email@testmail.com", userSchool);
+        expect(int.getSchool()).toBe(userSchool);
+        });
+    });
+    
+        
+});
+
+
+
+
+
+  
+  
