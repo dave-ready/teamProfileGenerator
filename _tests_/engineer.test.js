f3cd325a4363cc4cb00cea49ac6fabdb79da8a7b
dave ready <davis_ready@hotmail.com>
2021-02-01 22:16:09 -0500

    tests for getRole and getGithub functions

--- _tests_/engineer.test.js
+++ _tests_/engineer.test.js
@@ -6,6 +6,31 @@ describe("Engineer", () => {
         expect(typeOf(eng)).toBe("object");
     });
 
+
+    it("Will get Github account using constructor function", () => {
+        const userGithub = "Test_Github";
+        const eng = new Engineer("Dylan", 25, "test_email@testmail.com", userGithub);
+        expect(eng.github).toBe(userGithub);
+      });
+
+describe("getRole", () => {
+    it("Will return 'Engineer' using getRole function", () => {
+        const empRole = "Engineer";
+        const emp = new Engineer("Dylan", 25, "test_email@testmail.com", "Test_Github");
+        expect(emp.getRole()).toBe(empRole);
+    });
+
+});
+
+describe("getGithub", () => {
+    it("Will get employee's Github username using getGithub function", () => {
+        const userGithub = "Test_Github";
+        const eng = new Engineer("Dylan", 25, "test_email@testmail.com", "Test_Github" );
+        expect(eng.getGithub()).toBe(userGithub);
+    });
+});
+
+    
 });
 
 
